7173f3d69417d21eaad9170a407978a506e6928a
Edwin Syah <edwin.syah@trans-cosmos.co.id>
2025-12-09 10:56:25 +0700

    update pwa

--- app/layout.js
+++ app/layout.js
@@ -15,11 +15,22 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Ushalli",
   description: "Prayer Reminder in your Pocket",
+  manifest: "/manifest.json",
+  themeColor: "#000000",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    title: "Ushalli",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
