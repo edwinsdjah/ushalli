b9da6b15b799a7489362d4cd3cb95931a1776e8b
Edwin Syah <edwinsdjah@gmail.com>
2025-12-16 18:12:47 +0700

    tes

--- app/Components/Map/FlyTo.jsx
+++ app/Components/Map/FlyTo.jsx
@@ -1,11 +1,16 @@
-import { useEffect } from 'react';
 import { useMap } from 'react-leaflet';
+import { useEffect } from 'react';
 
 export default function FlyTo({ position }) {
   const map = useMap();
 
   useEffect(() => {
-    if (position) map.setView(position, 15);
+    if (!position) return;
+
+    map.flyTo(position, map.getZoom(), {
+      animate: true,
+      duration: 0.8,
+    });
   }, [position, map]);
 
   return null;
